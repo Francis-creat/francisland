e1a99af7860419e50acadf9470bafd43d1f69fab
Francis-creat <63809602+Francis-creat@users.noreply.github.com>
2020-05-06 22:51:28 -0300

    Add files via upload

---
+++ ProjetoBibliotecaLivroApplication.java
@@ -0,0 +1,13 @@
+package Landarin.Francis.projetoBibliotecaLivro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBibliotecaLivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBibliotecaLivroApplication.class, args);
+	}
+
+}
